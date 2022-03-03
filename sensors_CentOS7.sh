44ca002a9483ad733e2d95fa3d2ab3cf0ab675c1
Shota_22 <84522061+Melkor1998@users.noreply.github.com>
2022-03-03 21:35:32 +0400

    Add files via upload

---
+++ sensors_CentOS7.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+#CentOS 7
+
+#You must allow "less secure app access" to your gmail account, orelse script won't be able to authorize and can't show ur inbox
+
+yum list installed | grep sensors || yum install lm_sensors -y && sensors-detect
+which espeak || yum install espeak -y
+funct_login(){
+clear
+printf "username: "
+read username
+printf "Password: "
+read -s password
+if [[ $(curl -u $username:$password --silent "https://mail.google.com/mail/feed/atom" | grep -q Unauthorized && echo 1) == 1 ]];
+then
+	clear
+	echo -e "\e[91;1mAuthorization Failed\e[0m"
+	sleep 3
+	funct_login
+fi
+}
+funct_login
+funct_sensors(){
+	clear
+	sensors
+	printf "Time: \e[1m$(date +"%I:%M")\e[0m\n"
+	if [[ $b -gt 0 ]];
+	then
+		x='\e[91;1m'
+		y='\e[0m'
+	else
+		x=''
+		y=''
+	fi
+	printf "\nGmail notifications: $x$b$y\n"
+	printf "\e[91;1m"
+	#curl -u $username:$password --silent "https://mail.google.com/mail/feed/atom" | tr -d '\n' | awk -F '<entry>' '{for (i=2; i<=NF; i++    ) {print $i}}' | sed -n "s/<title>\(.*\)<\/title.*name>\(.*\)<\/name>.*/\2 - \1/p"
+	printf "$c"
+	printf "\e[0m"
+	a=$(curl -u $username:$password --silent "https://mail.google.com/mail/feed/atom" | tr -d '\n' | awk -F '<entry>' '{for (i=2; i<=NF; i++    ) {print $i}}' | sed -n "s/<title>\(.*\)<\/title.*name>\(.*\)<\/name>.*/\2 - \1/p")
+	b=$(curl -u $username:$password --silent "https://mail.google.com/mail/feed/atom" | tr -d '\n' | awk -F '<entry>' '{for (i=2; i<=NF; i++    ) {print $i}}' | sed -n "s/<title>\(.*\)<\/title.*name>\(.*\)<\/name>.*/\2 - \1/p" | wc -l)
+	if [[ $(echo $a | grep -q '-' && echo 1) -eq '1' ]];
+	then
+	c=$(curl -u $username:$password --silent "https://mail.google.com/mail/feed/atom" | tr -d '\n' | awk -F '<entry>' '{for (i=2; i<=NF; i++    ) {print $i}}' | sed -n "s/<title>\(.*\)<\/title.*name>\(.*\)<\/name>.*/\2 - \1/p")
+		espeak "You got mailed"
+	else
+		c=''
+	fi
+	sleep 1
+}
+while :
+do
+	funct_sensors
+done
