99a2dfd07517d880f34e8132e26b93232c3b9ac0
Shota_22 <84522061+Melkor1998@users.noreply.github.com>
2022-04-04 15:46:03 +0400

    Add files via upload

---
+++ play.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#You need to have ytfzf installed!
+#Make alias for script in .bashrc for example alias play="~/play.sh" and run > play "musicname..."
+music=$(echo $*)
+ytfzf -L $music > link
+clear
+echo -e "\e[1mNow playing:\e[0m $(youtube-dl --skip-download --get-title --no-warnings $(cat link) 2> /dev/null)"
+mpv --no-video --force-seekable=yes $(cat link) || clear && echo "Error"
+rm -rf link
