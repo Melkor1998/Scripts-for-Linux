e6fe038329f74d6236c63db5d6d226b5fd80dce6
Shota_22 <84522061+Melkor1998@users.noreply.github.com>
2022-04-08 21:20:02 +0400

    Update alarm.sh

--- alarm.sh
+++ alarm.sh
@@ -11,7 +11,7 @@ then
 	cd ~
 	clear
 	#We need ffmpeg to download file as mp3
-	sudo apt install ffmpeg -y; clear
+	apt install ffmpeg; clear
 	echo -e "\e[91mPlease wait... We need ringtone for alarm to work\e[0m"
 	youtube-dl --extract-audio --audio-format mp3 https://youtu.be/SmSeOMXIQ5U
 fi
@@ -19,7 +19,6 @@ fi
 #PLAYER
 which mpv &> /dev/null || sudo apt install mpv
 clear
-
 
 #Alarm
 printf "Set alarm to: "
@@ -38,7 +37,16 @@ fi
 sleeptime=$(( $alarm2 - $time ))
 timeleft=$(date -d@$sleeptime -u +%H:%M:%S)
 realtime=$(date "+%H:%M:%S")
-echo -e "Alarm is set to \e[96m$alarm\e[0m"
-echo -e "Time now is \e[96m$realtime\e[0m"
-echo -e "Time before alarm rings \e[96m$timeleft\e[0m"
-sleep $sleeptime && echo -e "\nPRESS \e[91;1mq\e[0m TO STOP\n" && mpv --loop ~/'Cool Ringtone-SmSeOMXIQ5U.mp3'
+
+while [[ $sleeptime -gt '0' ]]
+do
+	clear
+	sleeptime=$(( $sleeptime - 1 ))
+	timeleft=$(date -d@$sleeptime -u +%H:%M:%S)
+	echo -e "Will ring at  - $alarm"
+	echo -e "Time now is   - $(date +"%H:%M:%S")"
+	echo -e "Time left     - \e[92m$timeleft\e[0m"
+	sleep 1
+done
+
+clear && echo -e "\nPRESS \e[91;1mq\e[0m TO STOP\n" && mpv --loop ~/'Cool Ringtone-SmSeOMXIQ5U.mp3' && clear
